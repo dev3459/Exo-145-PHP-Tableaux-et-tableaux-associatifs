24b91c48a9e544b30715916595cd35711c7a451c
dev3459 <test45@outlook.fr>
2021-01-25 15:28:43 +0100

    Exo 145 Fini

--- index.php
+++ index.php
@@ -1,35 +1,35 @@
 <?php
 
 // Premiere ligne
-
-$fruits = array("Apple", "Banana" "Orange");
-echo ;
+$fruits = array("Apple", "Banana", "Orange");
+echo count($fruits)."<br>";
+echo $fruits[1];
 
 //Deuxieme ligne
+$age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
 
+echo "<br><br>Ben is " . $age['Ben'] . " years old.<br><br>";
 
-$age = array("Peter" "35", "Ben" "37", "Joe" "43");
-
-echo "<br><br>Ben is " .  . " years old.<br><br>";
-
-($age  $x  $y) {
-    echo "<br>Key=" .  . ", Value=" . ;
+foreach($age as $x => $y) {
+    echo "<br>Key=" . $x . ", Value=" . $y."<br>" ;
 }
 
 
 
 //Troisieme ligne
-
 $colors = array("red", "green", "blue", "yellow");
-;
-
+asort($colors);
+foreach($colors as $key => $val){
+    echo $key = $val."<br>";
+}
+arsort($colors);
+foreach($colors as $key => $val){
+    echo $key = $val."<br>";
+}
 
 // Quatrieme ligne
-
 $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
-;
-
-
-
-
-
+ksort($age);
+foreach($age as $key => $val){
+    echo $key = $val."<br>";
+}
